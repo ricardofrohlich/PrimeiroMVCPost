a1a3beb2296acffb3f9daea4487cc2d7d090ff3e
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2022-07-06 21:30:02 -0300

    Adicionar arquivos de projeto.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string nome, int vezes = 1, int ID = 1)
+        {
+
+            return HtmlEncoder.Default.Encode($"oi {nome}, vezes: {vezes}. ID: {ID}");
+        }
+        public string Teste()
+        {
+            return "aqui é só pra mostrar como funciona a relação rota x método dentro da controller";
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Pessoa p)
+        {
+            //string nome = p.Nome;
+            //string cpf = p.Cpf;
+            return View("Pessoa", p);
+        }
+
+        public IActionResult Pessoa(Pessoa p)
+        {
+            return View(p);
+        }
+
+        public IActionResult Cadastrar()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+   
+    }
+}
